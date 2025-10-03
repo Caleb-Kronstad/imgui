e597ea98d809046a854c6fb6109748c3fdca6006
Caleb-Kronstad <calebtkronstad@gmail.com>
2025-10-03 00:34:08 -0400

    Add Linux support to build

--- Build-IMGUI.lua
+++ Build-IMGUI.lua
@@ -1,54 +1,55 @@
 project "IMGUI"
-	kind "StaticLib"
-	language "C++"
+    kind "StaticLib"
+    language "C++"
 
-	targetdir ("../../../bin/" .. OutputDir .. "/%{prj.name}")
-	objdir ("../../../bin-int/" .. OutputDir .. "/%{prj.name}")
+    targetdir ("../../../bin/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../../../bin-int/" .. OutputDir .. "/%{prj.name}")
 
-	includedirs { 
+    includedirs {
         "%{IncludeDir.IMGUI}",
         "%{IncludeDir.GLAD}",
         "%{IncludeDir.GLFW}"
-	}
+    }
 
-	files {
-		"*.cpp",
-		"*.h",
-		"misc/cpp/*.cpp",
-		"misc/cpp/*.h",
-		"backends/imgui_impl_glfw.h",
-		"backends/imgui_impl_opengl3.h",
-		"backends/imgui_impl_glfw.cpp",
-		"backends/imgui_impl_opengl3.cpp"
-	}
+    files {
+        "*.cpp",
+        "*.h",
+        "misc/cpp/*.cpp",
+        "misc/cpp/*.h",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp"
+    }
 
-	defines {
-		"IMGUI_IMPL_OPENGL_LOADER_GLAD"
-	}
+    defines {
+        "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+    }
 
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+        defines {
+            "_CRT_SECURE_NO_WARNINGS"
+        }
 
-		defines { 
-			"_IMGUI_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-    
-	filter "system:linux"
-		pic "On"
+    filter "system:linux"
+        pic "On"
+        staticruntime "On"
 
-		systemversion "latest"
-		staticruntime "On"
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
 
-		defines	{
-			"_IMGUI_X11"
-		}
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
